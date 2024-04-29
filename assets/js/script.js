bfc280a25cb448aebda817078ca3d0918b7db556
MuhdNihalCY <mnihalcy@gmail.com>
2024-04-29 19:44:19 +0530

    created pricing page, but complete responsive is pending.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+// home page
 // Get the button element and the SVG path element
-const button = document.querySelector('.try-free-btn');
+const button = document.querySelector('.try-free-btn-1');
 const button_2 = document.querySelector('.try-free-btn-2');
 
 const pathElement = document.querySelector('#arrow-path');
@@ -10,22 +11,114 @@ const originalD = 'M21 7L1 7M21 7L15.0511 1M21 7L15.0511 13';
 const newD = 'M25 7L1 7M25 7L19.0511 1M25 7L19.0511 13';
 
 // Add event listeners to the button
-button.addEventListener('mouseover', () => {
-    // Change the d attribute to the new value when hovered
-    pathElement.setAttribute('d', newD);
-});
+if (button) {
+    button.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        pathElement.setAttribute('d', newD);
+    });
+    button.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        pathElement.setAttribute('d', originalD);
+    });
+}
 
-button.addEventListener('mouseout', () => {
-    // Revert the d attribute to the original value when not hovered
-    pathElement.setAttribute('d', originalD);
-});
+if (button_2) {
+    button_2.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        pathElement_2.setAttribute('d', newD);
+    });
 
-button_2.addEventListener('mouseover', () => {
-    // Change the d attribute to the new value when hovered
-    pathElement_2.setAttribute('d', newD);
-});
+    button_2.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        pathElement_2.setAttribute('d', originalD);
+    });
+}
 
-button_2.addEventListener('mouseout', () => {
-    // Revert the d attribute to the original value when not hovered
-    pathElement_2.setAttribute('d', originalD);
-});
+// pricing page
+const pricingFreeBtn = document.querySelector('.free-plan-btn');
+const pricingBasicBtn = document.querySelector('.basic-plan-btn');
+const pricingGrowthBtn = document.querySelector('.growth-plan-btn');
+const tryFreeBtnStremlinig = document.querySelector('.try-free-btn-stremlinig');
+
+const arrowPathPrFree = document.querySelector('#arrow-path-pr-free');
+const arrowPathPrBasic = document.querySelector('#arrow-path-pr-basic');
+const arrowPathPrGrowth = document.querySelector('#arrow-path-pr-growth');
+const arrowPathStremlining = document.querySelector('#arrow-path-stremlining');
+
+
+if (pricingFreeBtn) {
+    pricingFreeBtn.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        arrowPathPrFree.setAttribute('d', newD);
+    });
+    pricingFreeBtn.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        arrowPathPrFree.setAttribute('d', originalD);
+    });
+}
+
+if (pricingBasicBtn) {
+    pricingBasicBtn.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        arrowPathPrBasic.setAttribute('d', newD);
+    });
+    pricingBasicBtn.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        arrowPathPrBasic.setAttribute('d', originalD);
+    });
+}
+
+if (pricingGrowthBtn) {
+    pricingGrowthBtn.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        arrowPathPrGrowth.setAttribute('d', newD);
+    });
+    pricingGrowthBtn.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        arrowPathPrGrowth.setAttribute('d', originalD);
+    });
+}
+
+
+if (tryFreeBtnStremlinig) {
+    tryFreeBtnStremlinig.addEventListener('mouseover', () => {
+        // Change the d attribute to the new value when hovered
+        arrowPathStremlining.setAttribute('d', newD);
+    });
+    tryFreeBtnStremlinig.addEventListener('mouseout', () => {
+        // Revert the d attribute to the original value when not hovered
+        arrowPathStremlining.setAttribute('d', originalD);
+    });
+}
+
+
+
+
+const toggleBtn = document.getElementById('pricing-toggle-btn');
+if (toggleBtn) {
+    toggleBtn.addEventListener('change', () => {
+        const offerLabel = document.getElementById('pricing-save')
+        const annuallyPayLabel = document.getElementById('annually-label');
+        const montlyPayLabel = document.getElementById('monthly-label');
+
+        if (toggleBtn.value == "on") {
+            toggleBtn.value = "";
+            // console.log("montly");
+            document.getElementById('basic-price').textContent = "1099";
+            document.getElementById('growth-price').textContent = "3099";
+            
+            montlyPayLabel.classList.add('pricing-lable-active');
+            annuallyPayLabel.classList.remove('pricing-lable-active');
+            offerLabel.classList.remove('pricing-save-active');
+        } else {
+            toggleBtn.value = "on";
+            // console.log("annually");
+            document.getElementById('basic-price').textContent = "999";
+            document.getElementById('growth-price').textContent = "2999";
+
+            offerLabel.classList.add('pricing-save-active');
+            annuallyPayLabel.classList.add('pricing-lable-active');
+            montlyPayLabel.classList.remove('pricing-lable-active');
+        }
+    })
+}
